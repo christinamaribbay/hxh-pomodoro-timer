073820033a97c81d4af38d5cdeb395a37b863bac
tzzing <mmaribbay@ufl.edu>
2026-01-22 20:16:46 -0500

    removed default title bar, added font

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 380,
+    height: 500,
+    titleBarStyle: 'hidden',
+    roundedCorners: false
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+
+
